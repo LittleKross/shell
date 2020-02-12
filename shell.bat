882f4b97d6858d1ee62426635c11b53779f7b09f
Brandon Young <btyoung2k@gmail.com>
2020-02-12 16:13:00 -0500

    Adding versioning flag

--- shell.bat
+++ shell.bat
@@ -1,1 +1,1 @@
-python ./shell.py 1
+python ./shell.py -v
--- shell.py
+++ shell.py
@@ -1,7 +1,6 @@
 import sys
-print("hi")
+
 args = sys.argv
-out = "hi"
+
 if args[1] == "-v" or args[1] == "-V":
-    print("hi")
-print("hi")
+    print("Version 1.0.0")
