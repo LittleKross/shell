6cc23d0aef2098037cd2f99926ad7a15a3b671e8
Brandon Young <btyoung2k@gmail.com>
2020-05-03 14:43:48 -0400

    Cleaning ug code formatting

--- PythonNotes.md
+++ PythonNotes.md
@@ -28,3 +28,30 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.parse_args()
 ```
+
+### commented out code
+
+```python
+# def test():
+#     args = sys.argv
+
+#     Input processing
+#     test = fileinput.input()
+#     Flags
+#     if args.__len__() == 1 and test:
+#         sys.stdout.write("Help documentation...\n")
+#     else:
+#         Each case represents a flag at any specific element in the args array
+#         for i in range(1,args.__len__()):
+#             if args.__len__() == 1:
+#                 printStdIn()
+#             if args[i] == "-v" or args[i] == "-V" or args[i] == "--get-version":
+#                 sys.stdout.write("Version 1.0.0\n")
+#             if args[i] == "-h" or args[i] == "-H" or args[i] == "-?" or args[i] == "--Help" or args[i] == "--get-help":
+#                sys.stdout.write("Help documentation...")
+#             if args[i] == "-f" or args[i] == "-F" or args[i] == "--input-file" or args[i] == "--Input-File":
+#                 inputfile = open(args[i + 1])
+#                 break
+#             else:
+#                 inputfile = open(args[i])
+```
--- shell.py
+++ shell.py
@@ -7,11 +7,10 @@ import fileinput
 # Global variables
 version = "1.0.0"
 
+# Methods
 def parseInput():
     parser = argparse.ArgumentParser(add_help=False)
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("-v", "--version", action="store_true",help="Display current version info")
-    group.add_argument("-q", "--quiet", action="store_true",help="Display less information")
+    parser.add_argument("-v", "--version", action="store_true",help="Display current version info")
     parser.add_argument('-h', '--help',"-?", action='help', default=argparse.SUPPRESS,help='Show this help message and exit.')
     parser.add_argument("-f","--file", type=str, help="The file to output")
     args = parser.parse_args()
@@ -19,11 +18,10 @@ def parseInput():
 
 def evalInfo(argsData):
     if not sys.stdin.isatty():
-        test = fileinput.input()
-        if test.__sizeof__() != 0:
-            for line in fileinput.input():
-                print(line)
-        fileinput.close()
+        data = fileinput.input()
+        printStdIn(data)
+        data.close()
+        
 
     if argsData.file != None:
         datafile = open(argsData.file)
@@ -34,38 +32,16 @@ def evalInfo(argsData):
 def printFile(file):
     count = 0
     for line in file:
-            count += 1
             if count > 1:
-                print(line) # remove 1st two chars later
+                sys.stdout.write(line[3:])
+            count += 1
 
 def printStdIn(stdIn):
-    with stdIn as lines:
-        for line in lines:
-            if lines.lineno() != 1:
-                print(lines.lineno(),line)
+    for line in stdIn:
+            if stdIn.lineno() > 2:
+                sys.stdout.write(line[3:])
 
-# def test():
-    # args = sys.argv
-
-    # Input processing
-    # test = fileinput.input()
-    # Flags
-    # if args.__len__() == 1 and test:
-        # sys.stdout.write("Help documentation...\n")
-    # else:
-        # Each case represents a flag at any specific element in the args array
-        # for i in range(1,args.__len__()):
-            # if args.__len__() == 1:
-                # printStdIn()
-            # if args[i] == "-v" or args[i] == "-V" or args[i] == "--get-version":
-                # sys.stdout.write("Version 1.0.0\n")
-            #if args[i] == "-h" or args[i] == "-H" or args[i] == "-?" or args[i] == "--Help" or args[i] == "--get-help":
-            #    sys.stdout.write("Help documentation...")
-            # if args[i] == "-f" or args[i] == "-F" or args[i] == "--input-file" or args[i] == "--Input-File":
-                # inputfile = open(args[i + 1])
-                # break
-            # else:
-                # inputfile = open(args[i])
+# Main
 
 parsed = parseInput()
 evalInfo(parsed)
